443f56894838a7a255863cef73002aa0882b8abf
徐禄军 <lujun.xlj@alibaba-inc.com>
2018-06-15 19:34:18 +0800

    提交修改

---
+++ src/main/java/com/fun/iwork/IworkApplication.java
@@ -0,0 +1,12 @@
+package com.fun.iwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IworkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fun/iwork/IworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fun.iwork;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
